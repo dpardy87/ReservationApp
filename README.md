c3ce984a77fddcf0d55be74e5cc3f77a4963f781
dpardy87 <okcupescrusader@gmail.com>
2024-08-05 14:20:07 -0600

    set up basic flask web server

---
+++ README.md
@@ -0,0 +1,9 @@
+# Reservation App
+
+## build the image
+
+`docker build -t flask-app .`
+
+## run container
+
+`docker run -p 5001:5000 flask-app`
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return 'This is the about page.'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
